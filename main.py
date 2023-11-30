f319276f6c060c57a38eb2055b1c23c0076ed146
nabcellent <nabcellent.dev@gmail.com>
2023-11-30 08:59:36 +0300

    Updates cors middleware. Use import from starlette instead of fastapi
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import pandas as pd
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
 from app.recommend.custom import RecommendCustom
 from app.recommend.diet import RecommendDiet
@@ -22,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
