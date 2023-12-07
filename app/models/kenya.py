eee09e718468e4bd6c0ebb4e4e34992c292fabdb
nabcellent <nabcellent.dev@gmail.com>
2023-12-07 19:22:47 +0300

    Implements kenyan recipes recommendation
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- app/models/kenya.py
+++ app/models/kenya.py
@@ -1,17 +1,15 @@
 import numpy as np
-import re
 import polars as pl
-from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
 
 
 class KenyaModel:
+    dataset = pl.read_csv('data/ke-recipes.csv').fill_nan(None)
+
     def __init__(self, values):
         self._input = values
-        self.dataset = pl.read_csv('data/ke-recipes.csv')
-        self.dataset = self.dataset.fill_nan(None)
 
     def scaling(self, dataframe):
         scaler = StandardScaler()
@@ -31,40 +29,25 @@ class KenyaModel:
 
         return pipeline
 
-    def extract_data(self, dataframe, ingredients):
-        extracted_data = dataframe.clone()
-        extracted_data = self.extract_ingredient_filtered_data(extracted_data, ingredients)
-
-        return extracted_data
-
-    def extract_ingredient_filtered_data(self, dataframe, ingredients):
-        extracted_data = dataframe.clone()
-
-        return extracted_data.filter([pl.col('ingredients').str.contains(i) for i in ingredients])
-
     def apply_pipeline(self, pipeline, _input, extracted_data):
         _input = np.array(_input).reshape(1, -1)
 
         return extracted_data[pipeline.transform(_input)[0]]
 
     def recommend(self, _input, ingredients, params):
-        extracted_data = self.extract_data(self.dataset, ingredients)
+        data = self.dataset
 
-        if extracted_data.shape[0] >= params['n_neighbors']:
-            prep_data, scaler = self.scaling(extracted_data)
-            neigh = self.nn_predictor(prep_data)
-            pipeline = self.build_pipeline(neigh, scaler, params)
+        if len(ingredients) > 0:
+            data = data.filter([pl.col('ingredients').str.contains(i) for i in ingredients])
 
-            return self.apply_pipeline(pipeline, _input, extracted_data)
-        else:
+        if data.shape[0] < params['n_neighbors']:
             return None
 
-    def extract_quoted_strings(self, s):
-        # Find all the strings inside double quotes
-        strings = re.findall(r'"([^"]*)"', s)
-        # Join the strings with 'and'
+        prep_data, scaler = self.scaling(data)
+        neigh = self.nn_predictor(prep_data)
+        pipeline = self.build_pipeline(neigh, scaler, params)
 
-        return strings
+        return self.apply_pipeline(pipeline, _input, data)
 
     def output_recommended_recipes(self, dataframe):
         if dataframe is not None:
@@ -74,7 +57,6 @@ class KenyaModel:
             for recipe in output:
                 recipe['ingredients'] = recipe['ingredients'].split(';')
                 recipe['instructions'] = recipe['instructions'].split(';')
-        else:
-            output = None
 
-        return output
+            return output
+        return None
